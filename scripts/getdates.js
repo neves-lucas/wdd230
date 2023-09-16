eccb593315dd870a15027baca267296909a46f62
Lucas <neves.lucas.r@gmail.com>
2023-09-15 21:08:02 -0300

    Course home page done

---
+++ scripts/getdates.js
@@ -0,0 +1,17 @@
+let currentYear = new Date().getFullYear();
+let modifiedDate = new Date();
+let dateElement = document.getElementById('lastModified')
+const options = {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+};
+
+const formattedDate = modifiedDate.toLocaleDateString('en-US', options);
+
+
+document.getElementById("year").innerHTML = currentYear;
+dateElement.innerHTML = `<strong>Last Modified: ${formattedDate}</strong>`;
