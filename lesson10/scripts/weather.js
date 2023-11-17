7c5a764a15bc4ecf9f3bff745164d2c2620e5757
Lucas <neves.lucas.r@gmail.com>
2023-11-16 21:32:07 -0300

    Weather activity done

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,33 @@
+// select HTML elements in the document
+  const currentTemp = document.querySelector('#current-temp');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74968760946712&lon=6.63784400597848&appid=27d6695b0906dbabb83349e0d5e3a366&units=imperial';
+
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data); 
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data)  {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
